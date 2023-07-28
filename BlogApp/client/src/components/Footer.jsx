50f324480fb910797711966b3c339b3459382839
Danish Minhas <53962550+danishminhas1@users.noreply.github.com>
2023-07-28 16:52:38 +0500

    Add files via upload

---
+++ BlogApp/client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo2.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Software Construction Semester Project
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
